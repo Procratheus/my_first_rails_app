febd899706216d41ef2188c51cc0976b49019a03
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2014-10-27 23:43:31 +0000

    Fixed Syntax Error

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -5,7 +5,7 @@ if Rails.env.development?
     port:           "587", 
     authentication: :plain,
     user_name:      ENV["SEND_USERNAME"],
-    password:       ENV["SEND_PASSWORD"] 
+    password:       ENV["SEND_PASSWORD"], 
     domain:         "heroku.com",
     enable_starttls_auto: true 
   }
