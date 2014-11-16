c402c0a8d935e569c0341e8b06649916666d44c4
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2014-11-16 19:15:00 +0000

    Refactored Post/Topic controllers and added a User controller

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -10,7 +10,7 @@ class TopicsController < ApplicationController
   end
 
   def create
-    @topic = Topic.new(params.require(:topic).permit(:name, :description, :public))
+    @topic = Topic.new(topics_params)
     authorize @topic
     if @topic.save
       redirect_to @topic, notice: "Topic was saved successfully."
@@ -34,11 +34,18 @@ class TopicsController < ApplicationController
   def update
     @topic = Topic.find(params[:id])
     authorize @topic
-    if @topic.update_attributes(params.require(:topic).permit(:name, :description, :public))
+    if @topic.update_attributes(topics_params)
       redirect_to @topic
     else
       flash[:error] = "There was an error updating this topic"
       render :edit
     end
   end
+
+  private
+
+  def topics_params
+    params.require(:topic).permit(:name, :description, :public)
+  end
+
 end
