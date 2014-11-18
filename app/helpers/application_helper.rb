be635c498748e44a2725b9051403e89da8599730
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2014-11-18 00:02:43 +0000

    Final commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
 
-  def form_group_tag(errors, &block)
+  def form_group_tag(errors)
     if errors.any?
-      content_tag :div, capture(&block), class: 'form-group has-error'
+      class_tag_error = "form-group has-error"
     else
-      content_tag :div, capture(&block), class: 'form-group'
+      class_tag_error = "form-group"
     end
   end
 
