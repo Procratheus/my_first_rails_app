6caab181c0b06746e8bb92ff589ecd95a2b93061
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2014-11-29 12:07:25 +0000

    Deleted comment variable from post_controllr.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,6 @@ class PostsController < ApplicationController
   def show
     @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
-    @comment = @post.comments.all
   end
 
   def new
