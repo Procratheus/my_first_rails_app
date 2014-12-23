69ada6a6f422fc6f6d3512e49ad345d456f789b2
Alexander Lewis <alex.lewis79@yahoo.co.uk>
2014-12-23 21:58:13 +0000

    completed FE another interlude

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,5 +18,19 @@ module ApplicationHelper
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render text).html_safe
   end
+
+  def vote_link_classes(post, string)
+    if string == "up" && current_user.voted(post) && current_user.voted(post).up_vote?    
+      "glyphicon glyphicon-chevron-up voted"
+    elsif string == "up"
+      "glyphicon glyphicon-chevron-up"
+    elsif string == "down" && current_user.voted(post) && current_user.voted(post).down_vote?
+      "glyphicon glyphicon-chevron-down voted"
+    elsif string == "down"
+      "glyphicon glyphicon-chevron-down"
+    else
+      " "
+    end    
+  end
   
 end
